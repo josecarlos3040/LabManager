e35101c08c0026d194a6ee136691a543e4b962a5
José Carlos <102222057+josecarlos3040@users.noreply.github.com>
2022-05-31 19:37:05 -0300

    added method that shows computer searching by id

--- LabManager/Program.cs
+++ LabManager/Program.cs
@@ -5,9 +5,46 @@ using LabManager.Models;
 
 var databaseConfig = new DatabaseConfig();
 
+var DatabaseSetup = new DatabaseSetup(databaseConfig);
+
+var computerRepository = new ComputerRepository(databaseConfig);
+
+var modelName = args[0];
+
+var modelAction = args[1];
+
+
+if (modelName == "Computer")
+{
+    if (modelAction == "List")
+    {
+        Console.WriteLine("Computer List");
+        foreach (var computer in computerRepository.GetAll())
+        {
+            Console.WriteLine($"{computer.Id}, {computer.Ram}, {computer.Processor}");
+        }
+    }
+    if (modelAction == "New")
+    {
+        var id = Convert.ToInt32(args[2]);
         var ram = args[3];
         var processor = args[4];
-
         var computer = new Computer(id, ram, processor);
 
         computerRepository.Save(computer);
+    }
+
+    if (modelAction == "Show")
+    {
+        try
+        {
+            var id = Convert.ToInt32(args[2]);
+            var computer = computerRepository.GetById(id);
+            Console.WriteLine($"{computer.Id}, {computer.Ram}, {computer.Processor}");
+        }
+        catch (System.Exception)
+        {
+            Console.WriteLine("Input value is not valid (id may not exist)");
+        }
+    }
+}
--- LabManager/Repositories/ComputerRepository.cs
+++ LabManager/Repositories/ComputerRepository.cs
@@ -2,6 +2,7 @@ using LabManager.Database;
 using LabManager.Models;
 using Microsoft.Data.Sqlite;
 namespace LabManager.Repositories;
+
 class ComputerRepository
 {
     private readonly DatabaseConfig _databaseConfig;
@@ -9,7 +10,6 @@ class ComputerRepository
     {
         _databaseConfig = databaseConfig;
     }
-
     public List<Computer> GetAll()
     {
         var computers = new List<Computer>();
@@ -21,19 +21,21 @@ class ComputerRepository
         while (reader.Read())
         {
             var id = reader.GetInt32(0);
+
             var ram = reader.GetString(1);
+
             var processor = reader.GetString(2);
+            
             var computer = new Computer(id, ram, processor);
             computers.Add(computer);
         }
         connection.Close();
-
         return computers;
     }
 
     public Computer Save(Computer computer)
     {
-        var connection = new SqliteConnection("Data Source=database.db");
+        var connection = new SqliteConnection(_databaseConfig.ConnectionString);
         connection.Open();
 
         var command = connection.CreateCommand();
@@ -41,8 +43,28 @@ class ComputerRepository
         command.Parameters.AddWithValue("$id", computer.Id);
         command.Parameters.AddWithValue("$ram", computer.Ram);
         command.Parameters.AddWithValue("$processor", computer.Processor);
-
         command.ExecuteNonQuery();
+        connection.Close();
+
+        return computer;
+    }
+
+    public Computer GetById(int id)
+    {
+        var connection = new SqliteConnection(_databaseConfig.ConnectionString);
+        connection.Open();
+
+        var command = connection.CreateCommand();
+        command.CommandText = "SELECT * FROM Computers WHERE id = ($id)";
+        command.Parameters.AddWithValue("$id", id);
+
+        var reader = command.ExecuteReader();
+        reader.Read();
+
+        var ram = reader.GetString(1);
+        var processor = reader.GetString(2);
+        var computer = new Computer(id, ram, processor);
+
         connection.Close();
 
         return computer;
